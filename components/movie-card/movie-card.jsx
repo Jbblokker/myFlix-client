6d5fc39ca9cc83db91a2d2dadaef278ca19f562c
Jbblokker <jbblokker@outlook.com>
2021-04-20 23:01:21 -0500

    added movie-card.jsx to project

---
+++ components/movie-card/movie-card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class MovieCard extends React.Component {
+    render() {
+        const { movie, onMovieClick } = this.props;
+        return <div className="movie-card" onClick={()=> { this.state.selectedMovie = movie; }}
+            >{movie.Title}</div>;
+        
+    }
+}
