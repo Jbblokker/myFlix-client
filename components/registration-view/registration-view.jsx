6ab038805c4e720ea60db55b7e89f1b168da6ab5
Jbblokker <jbblokker@outlook.com>
2021-04-25 15:24:31 -0500

    added registration view file

---
+++ components/registration-view/registration-view.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function RegistrationView(props) {
+    const [ Username, setUsername ] = useState('');
+    const [ Password, setPassword ] = useState('');
+    const [ Email, setEmail ] = useState('');
+    const [ Birthday, setBirthday] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password, email,birthday);
+        props.onRegister(username); 
+    }
+
+    
+        return(
+            <Container>
+          <form>
+            <label>
+              Email:
+              <input type='text' value={Email} onChange={e => setEmail(e.target.value)} />
+            </label>
+            <label>
+              Birthday:
+            <input type='text' value={Birthday} onChange={e => setBirthday(e.target.value)} />
+            </label>
+            <label>
+             Username:
+             <input type='text' value={Username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+            Password:
+            <input type='text' value={Password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="button" onClick={this.handleSubmit}>Submit</button>
+          </form> 
+          </Container>
+        );
+}
