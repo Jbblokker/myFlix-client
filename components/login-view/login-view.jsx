5c0e2b95a75c18e0d281234b8e8d3328d027d5fb
Jbblokker <jbblokker@outlook.com>
2021-04-27 22:27:12 -0500

    added form to return in login-view

--- components/login-view/login-view.jsx
+++ components/login-view/login-view.jsx
@@ -15,18 +15,19 @@ export function LoginView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => 
-                setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e =>
-                setPassword(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
+        <Form>
+            <Form.Group controlId="formUsername">
+                <Form.Label Username />
+                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label Password />
+                <Form.Control type= "password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
+        </Form>
     );               
 }
