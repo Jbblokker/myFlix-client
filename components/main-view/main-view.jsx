3afe113e474f58d02056e36e7139fb4c28339960
Jbblokker <jbblokker@outlook.com>
2021-04-27 22:28:51 -0500

    remove div for making better reading
    added row with minor styling for center of page

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -59,7 +59,7 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view"/>;
 
         return(
-            <div className="main-view">
+          <Row className="main-view justify-content-md-center">
             {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
