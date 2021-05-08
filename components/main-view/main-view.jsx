0d0b105153b6925781802854aa5834a49f7d77cf
Jbblokker <jbblokker@outlook.com>
2021-05-08 10:58:06 -0500

    Update main-view.jsx

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -22,6 +22,7 @@ export class MainView extends React.Component {
         };
     }
     
+    
     componentDidMount(){
         axios.get('https://sleepy-crag-80436.herokuapp.com/movies')
         .then(response => {
