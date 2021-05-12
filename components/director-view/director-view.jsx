882d687faf37b08990ab7e87b50fc252b724ed5b
Jbblokker <jbblokker@outlook.com>
2021-05-12 06:09:13 -0500

    formating display with Bootstrap

--- components/director-view/director-view.jsx
+++ components/director-view/director-view.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+//styling from bootstrap
+import { Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import './director-view.scss';
 
 export class DirectorView extends React.Component {
+
+
 
     render() {
         const { description, onBackClick }= this.props;
         
         return (
             <div className="director-view">
-                <div className="director-name">
-                    <span className="label">Name:</span>
-                    <span className="value">{Director.Name}</span>
-                </div>
-                <div className="director-bio">
-                    <span className="label">Bio</span>
-                    <span className="value">{Director.Bio}</span>
-                </div>
-            </div>    
+                <Card className="director-card">
+                    <Card.Title className='director-title'>DIRECTOR</Card.Title>
+                        <div className='director-name'>
+                             <span className="label">Name: </span>
+                              <span className="value">{director.Name}</span>
+                        </div>
+                        <div className="director-bio">
+                              <span className="label">Bio: </span>
+                              <span className="value">{director.Bio}</span>
+                        </div>
+                        <div className='director-back'>
+                            <Button variant="primary" onClick={() => onBackClick()}>
+                                Back
+                            </Button>
+                        </div>
+                </Card>
+             </div>    
+           </Container>
         );
     }
 }
+
+
