f16e9c79135f51fbbdba5521b946cc26c79414c5
Jbblokker <jbblokker@outlook.com>
2021-05-12 06:18:13 -0500

    create ProfileView

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -1,45 +1,86 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 //Bootstrap components
-import Buttom from 'react-bootstratp/Button';
-import Form from 'react-bootstrat/Form';
+import Buttom from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 //import styling 
 import './profile-view.scss';
 
-//export profile-view 
-export function ProfileView(props){
+//export
+export class ProfileView extends React.Compnent {
+    constroctor(props) {
+        super(props);
+        this.state = {
+            username:'',
+            password:'',
+            email:'',
+            birthday:'',
+            favoriteMovies:''
+        };
+    }
+
+    componentDidMount(){
+        this.GetUser();
+    }
+
+    getUser() {
+        const username = localStorage.getItem('user');
+        let token = localStorage.getItem('token');
+
+        axios.get(`https://sleepy-crag-80436.herokuapp.com/users/${username}`, {
+            headers: { Authorization: `Bearer ${token}`},
+        })
+
+        .then((response) => {
+            this.setState({
+                Username:response.data.username,
+                password:response.data.password,
+                email:response.data.email,
+                birthday:response.data.birthday
+            })
+        })
+
+        .catch(function (error) { 
+            console.log('error at getUser', error);
+        })
+    }
+
+    handleDeregister(e) {
+        e.preventDefault();
+
+        const token = localStorage.getItem('token');
+        const username = localStorage.getItem('user');
+
+        axios.delete('https://sleepy-crag-80436.herokuapp.com/users/${username}'{
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(() => {
+            localStorage.removeItem('user');
+            localStorage.removeItem('token');
+        })
+        .catch((e) => { 
+            console.log(e);
+        });
+
+
+
+    }
+
+    handleUpdate(e, username, password, email, birthday ) {
+
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        const username = loaclStorage.getItem('user');
+
+        const
+    }
 
 }
-    //send a request to the server to retrieve user info 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Send a request to the server for authentication 
-        axios.delete('https://sleepy-crag-80436.herokuapp.com/user', {
-            headers: { Authorization: `Bearer ${token}`}
-    })
-        .then(response => {
-            response.data("User has been deleted.")
-            props.onLoggedIn(data);
-        })
-        .catch(e => {
-            console.log('user cannot be deleted');
-            console.log(e);
-        })
-       
-        axios.get('https://sleepy-crag-80436.herokuapp.com/users/movieiID, {
-            
-        .then(repsonse)// i need to be able to pull the users fav movie id
-        })
 
-        .catch(e => {
-            console.log('cannot pull favorite movies');
-            console.log(e);
-        })
-//for profile view I need to look into handle.sumbit and axios to get the full 
-// idea how to creat this page. 
-// lets draw it out and see if we can create it too
 
 
 // I need to be able for user to update their info
