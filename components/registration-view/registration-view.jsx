72482f3963556496d332abbaca5d28d80228c964
Jbblokker <jbblokker@outlook.com>
2021-05-12 22:58:52 -0500

    Update registration-view.jsx

--- components/registration-view/registration-view.jsx
+++ components/registration-view/registration-view.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import PropTypes from 'prop-types';
-import { Container} from 'react-bootstrap/Container';
-import { Form } from 'react-bootstrap/Form';
- 
-export default function RegisterView(props) {
+import Container from 'react-bootstrap/Container';
+import Form  from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+export function RegisterView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
@@ -17,7 +19,7 @@ export default function RegisterView(props) {
         //props.onRegister(username); 
     }
     
-    axios.post('https://sleepy-crag-80436.herokuapp.com/users', {
+    axios.post('https://sleepy-crag-80436.herokuapp.com/user', {
       Username: username,
       Password: password,
       Email: email,
@@ -40,19 +42,19 @@ export default function RegisterView(props) {
               <Form>
                 <Form.Group controlId="regEmail">
                   <Form.Label>Email:</Form.Label>
-                  <Form.Control type='text' value={Email} onChange={e => setEmail(e.target.value)} />
+                  <Form.Control type='text' value={email} onChange={e => setEmail(e.target.value)} />
                 </Form.Group>
                 <Form.Group controlId="regBirthday">
                   <Form.Label>Birthday:</Form.Label>
-                  <Form.Control type='text' value={Birthday} onChange={e => setBirthday(e.target.value)} />
+                  <Form.Control type='text' value={birthday} onChange={e => setBirthday(e.target.value)} />
                 </Form.Group>
                 <Form.Group controlId="regUser">
                   <Form.Label>Username:</Form.Label>
-                  <Form.Control type='text' value={Username} onChange={e => setUsername(e.target.value)} />
+                  <Form.Control type='text' value={username} onChange={e => setUsername(e.target.value)} />
                 </Form.Group>
-                <Form.Group Controlid="regPassword">
+                <Form.Group controlId="regPassword">
                   <Form.Label>Password:</Form.Label>
-                  <Form.Control type='text' value={Password} onChange={e => setPassword(e.target.value)} />
+                  <Form.Control type='text' value={password} onChange={e => setPassword(e.target.value)} />
                 </Form.Group>
                 <Button variant="primary" type="submit" onClick={ onSubmit }> SUBMIT </Button>
               </Form> 
