8b70510792d49d189be26fbecc9b74c8cf9f9186
Jbblokker <jbblokker@outlook.com>
2021-05-16 05:32:23 -0500

    Update movie-view.jsx

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
     render() {
-        const { movie, director,  onBackClick }= this.props;
+        const { movie, director, onBackClick }= this.props;
 
         return (         
-            <div className="movie-view">
+            <Card className="card-poster">
                 <div className="movie-poster">
                     <img src={movie.ImagePath} />
                 </div>
@@ -18,22 +20,24 @@ export class MovieView extends React.Component {
                     <span className="value">{movie.Title}</span>
                 </div>
                 <div className="movie-description">
-                    <span className="label">Description: </span>
+                    <span className="label-Des">Description: </span>
                     <span className="value">{movie.Description}</span>
                 </div>
 
                 <Link to={`/directors/${movie.Director.Name}`}>
-                        <Button variant="link"> Director </Button>
+                        <Button variant="primary"> Director </Button>
                     </Link> 
                     
                     <Link to={`/genres/${movie.Genre.Name}`}>
-                        <Button variant="secondary"> Genre </Button>
+                        <Button variant="primary"> Genre </Button>
                 </Link>
+                <div class="backbutton">
                 <Link to={`/`}>
-                        <Button className='button-back' variant='primary'>Back</Button>
+                        <Button className='button-back' variant='danger'>Back</Button>
                 </Link>
+                </div>
                
-            </div>
+            </Card>
         );
     }
 }
